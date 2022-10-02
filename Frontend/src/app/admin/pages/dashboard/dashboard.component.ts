f1b70ffbf5dfce46851f6c0e73c6273a2e35faae
jb-abhi <jbabhi2@gmail.com>
2022-10-02 16:34:00 +0530

    dashboard ongoing

--- Frontend/src/app/admin/pages/dashboard/dashboard.component.ts
+++ Frontend/src/app/admin/pages/dashboard/dashboard.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { Renderer2 } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
+  host: {
+    '(document:click)': 'onClick($event)',
+  },
 })
 export class DashboardComponent implements OnInit {
+  names: string[] = [
+    'Monicca James',
+    'Samantha',
+    'Catherine',
+    'Monica James',
+    'Ileana Sam',
+  ];
+  constructor(private render: Renderer2, private _eref: ElementRef) {}
+  prevParentEl: any;
+  ngOnInit(): void {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onClick(event: any) {
+    if (this.prevParentEl && !this._eref.nativeElement.contains(event.target)) {
+      this.render.removeClass(this.prevParentEl, 'table-active');
+    }
   }
 
+  onRowClick(event: any, name: any, i: number) {
+    if (this.prevParentEl) {
+      this.render.removeClass(this.prevParentEl, 'table-active');
+    }
+    this.render.addClass(event.target.parentElement, 'table-active');
+    console.log(event.target.parentElement);
+    console.log(name, i);
+    this.prevParentEl = event.target.parentElement;
+  }
 }
