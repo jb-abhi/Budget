b7bada8faa81c3bd1f7457ad2061159697569a61
jb-abhi <jbabhi2@gmail.com>
2022-10-02 10:27:12 +0530

    dashboard ongoing

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
+import { SidenavComponent } from './admin/sidenav/sidenav.component';
+import { DashboardComponent } from './admin/pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    AdminComponent,
+    SidenavComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
